308f7461ef2b4aacc7144c50ef7dd83c497919b2
Austin Atwood <austin.w.atwood@gmail.com>
2019-09-07 07:58:21 +0900

    Added test objects

--- tourist.py
+++ tourist.py
@@ -1,1 +1,3 @@
-print("hello")
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
