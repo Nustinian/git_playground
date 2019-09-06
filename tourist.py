17098be4fa93f6c8e5e3fb08d99ffffdc691acb3
Austin Atwood <austin.w.atwood@gmail.com>
2019-09-07 08:20:53 +0900

    Added interest finder logic

--- tourist.py
+++ tourist.py
@@ -22,6 +22,16 @@ def add_attraction(destination, attraction):
     attractions_for_destination.append(attraction)
     return
 
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        for interest in interests:
+            if interest in attraction[1]:
+                attractions_with_interest.append(attraction[0])
+    return attractions_with_interest
+
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -33,4 +43,6 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print(attractions)
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
