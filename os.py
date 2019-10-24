ec70a91cddd6a7cdaa9abb7cf2807baa7d411300
Cloud User <centos@ip-172-31-46-205.ap-northeast-1.compute.internal>
2019-10-25 08:57:19 +0900

    yes

--- os.py
+++ os.py
@@ -1,8 +1,24 @@
 import os
 
-myCmd = 'ls -la'
-output = os.system(myCmd)
+def get_warnings_total():
+    cmd = "sudo cat -n /var/log/awslogs.log" | grep "WARNING" | wc -n"
+    total = int(os.popen(cmd).read())
+    return total
 
-input = "echo " + output + ">> output.txt")
+def search_and_add(thread_number):
+    thread_name = f(Thread-{thread_number})
+    cmd = f'sudo cat -n /var/log/awslogs.log | grep "WARNING" | grep "{thread_name}" | wc -l'
+    warnings_num = int(os.popen(cmd).read())
+    remaining_warnings -= warnings_num
+    threads_dict[thread_name] = warnings_num
 
-os.system(input)
+    
+
+threads_dict = {}
+
+myCmd = 'ls -la > output.txt'
+os.system(myCmd)
+
+warnings_total = get_warnings_total()
+remaining_warnings = warnings_total
+
