17d56d682deea34f5d9bfc3e1e9b67bb5a15d301
unknown <austin.w.atwood@gmail.com>
2019-10-25 07:54:54 +0900

    yes

--- psutil_test.py
+++ psutil_test.py
@@ -1,1 +1,16 @@
-print("hello world")
+import psutil
+from pathlib import Path
+import datetime
+
+def date(format="%Y%m%d"):
+    return datetime.datetime.utcnow().strftime(format)
+
+def make_output_dir() -> Path:
+    today = date("%Y%m%d")
+    output_dir = Path(".") / f"results_{today}"
+    output_dir.mkdir(exist_ok=True)
+    return output_dir
+
+if __name__ == "__main__":
+    output_dir = make_output_dir()
+    
