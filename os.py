8b0f43eba7d2ea914770382d0b0738ebd8a34105
unknown <austin.w.atwood@gmail.com>
2019-11-05 08:08:58 +0900

    i believe it

--- os.py
+++ os.py
@@ -6,8 +6,8 @@ def get_warnings_total():
     return total
 
 def search_and_add(thread_number):
-    thread_name = f(Thread-{thread_number})
-    cmd = f'sudo cat -n /var/log/awslogs.log | grep "WARNING" | grep "{thread_name}" | wc -l'
+    thread_name = "Thread-{thread_number}".format(thread_number=thread_number)
+    cmd = 'sudo cat -n /var/log/awslogs.log | grep "WARNING" | grep "{thread_name}" | wc -l'.format(thread_number=thread_number)
     warnings_num = int(os.popen(cmd).read())
     remaining_warnings -= warnings_num
     threads_dict[thread_name] = warnings_num
@@ -15,9 +15,6 @@ def search_and_add(thread_number):
     
 
 threads_dict = {}
-
-myCmd = 'ls -la > output.txt'
-os.system(myCmd)
 
 warnings_total = get_warnings_total()
 remaining_warnings = warnings_total
