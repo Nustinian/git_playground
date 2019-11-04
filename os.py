14b76d39b87fdf10611468ea2ec3ad96c7860741
unknown <austin.w.atwood@gmail.com>
2019-11-05 08:15:14 +0900

    moved some stuff

--- os.py
+++ os.py
@@ -5,6 +5,9 @@ def get_warnings_total():
     total = int(os.popen(cmd).read())
     return total
 
+warnings_total = get_warnings_total()
+remaining_warnings = warnings_total
+
 def search_and_add(thread_number):
     thread_name = "Thread-{thread_number}".format(thread_number=thread_number)
     cmd = 'sudo cat -n /var/log/awslogs.log | grep "WARNING" | grep "{thread_name}" | wc -l'.format(thread_name=thread_name)
@@ -12,12 +15,7 @@ def search_and_add(thread_number):
     remaining_warnings -= warnings_num
     threads_dict[thread_name] = warnings_num
 
-    
-
 threads_dict = {}
-
-warnings_total = get_warnings_total()
-remaining_warnings = warnings_total
 
 print("There are " + str(warnings_total) + " warnings in the awslogs.log.")
 search_and_add(1)
