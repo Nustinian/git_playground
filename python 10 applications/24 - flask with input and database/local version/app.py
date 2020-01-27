057494b1c0f31591be07ea8ef1c03f5bd0b99a67
unknown <austin.w.atwood@gmail.com>
2020-01-27 23:49:07 +0900

    200127 - added random upload and download functionality

--- python 10 applications/24 - flask with input and database/local version/app.py
+++ python 10 applications/24 - flask with input and database/local version/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import backend
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 db = backend.Database()
@@ -10,12 +11,22 @@ def index():
 
 @app.route('/success', methods=['POST'])
 def success():
+  global cake
   if request.method == 'POST':
     email = request.form["email_name"]
     height = request.form["height_value"]
+    cake = request.files["file"]
+    print(cake.filename)
+    cake.save(secure_filename("uploaded-" + cake.filename))
+    with open(secure_filename("uploaded-" + cake.filename), "a") as file:
+      file.write("\nthis was added later!")
     db.insert(email, height)
     backend.send_email(email, db.return_average_height())
-    return render_template("success.html")
+    return render_template("success.html", btn="download.html")
+
+@app.route('/download')
+def download():
+  return send_file(secure_filename("uploaded-" + cake.filename), attachment_filename="yourfile.txt", as_attachment=True)
 
 if __name__ == "__main__":
   app.run(debug=True)
